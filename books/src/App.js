c43198dbcbe9ed53064732b5558ac1d9fbd7e505
joemc9011 <joemc9011@gmail.com>
2021-02-06 17:22:55 -0500

    more html routes

--- books/src/App.js
+++ books/src/App.js
@@ -1,18 +1,25 @@
 import Nav from "./components/Nav";
-import Main from "./components/pages/Main"
 import Saved from "./components/pages/Saved";
 import Search from "./components/pages/Search";
+import Wrapper from "./components/Wrapper";
+
 import {BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
-    <Main/>
-    <Nav/>
-    <Saved/>
-    <Search/>
-    </>
-  )
+    <Router>
+       <div>
+         <Nav/>
+         <Wrapper>
+         <Route exact path="/" component={Search} />
+         <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+         </Wrapper>
+    </div>
+    </Router>
+   
+  );
 }
 
 export default App;
