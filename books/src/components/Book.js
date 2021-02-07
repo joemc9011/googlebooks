6a0cf032174e6dc592f49aeed1fae2af977cdcbe
joemc9011 <joemc9011@gmail.com>
2021-02-06 19:00:29 -0500

    small backend stuff

--- books/src/components/Book.js
+++ books/src/components/Book.js
@@ -4,15 +4,26 @@ import React from "react";
 function Book() {
     return (
         <div>
-            <h3>Book title</h3>
-            <p>Synopsis</p>
-            <p>author</p>
-            <p>image tag here</p>
-        <span>
-                Book info paragragh
-        </span>
-            <button>View</button>
-            <button>Delete</button>
+            <div className="card text-center">
+                <div className="card-header">
+                    <h3>Book Title</h3>
+                    <p>Synopsis</p>
+                    <p>Author</p>
+                </div>
+                <div className="card-body">
+                    <div className="col-md-4">
+                        <img src="{...}" alt="..." />
+                    </div>
+                    <p className="card-text"> Book info paragragh</p>
+                    <a href="#" className="btn btn-primary">View</a>
+                    <a href="#" className="btn btn-primary">Delete</a>
+                </div>
+
+            </div>
+
+
+
+
         </div>
 
     );
--- books/src/components/Booksearch.js
+++ books/src/components/Booksearch.js
@@ -1,9 +1,17 @@
-function Booksearch() {
+function Booksearch(props) {
     return (
         <div>
              <h5>Book Search</h5>
-        <input type="text" placeholder="Please type book here"></input>
-        <button>Search</button>
+        <input 
+        type="text"
+        placeholder="Please type book here"
+        value={props.search}
+        onChange={props.handleInputChange}
+        name="books"
+        className="booksearch"
+        id="booksearchid"
+        />
+        <button classname = "searchbutton" type="Submit" onClick={props.handleFormSubmit}> Search</button>
         </div>
        
 
