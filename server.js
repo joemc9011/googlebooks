f1c5c23272b9e7dd94380b5749264ae40dca2106
joemc9011 <joemc9011@gmail.com>
2021-02-13 19:09:17 -0500

    seed

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require ("express");
 const path = require ("path");
+const mongoose = require ("mongoose");
+const routes = require ("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,9 +12,18 @@ if (process.env.NODE_ENV ==="production") {
     app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+
+app.use(routes)
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
   
   app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
